94a56cd831a64db4ef594f0d74e949317f770382
Agung Kurniawan <agungk878@gmail.com>
2025-05-13 17:16:09 +0700

    tambah komentar

--- sw.js
+++ sw.js
@@ -1,23 +1,27 @@
+// Menambahkan event listener untuk proses instalasi service worker
 self.addEventListener('install', event => {
-    // wait until the service worker is installed
+    // Tunggu hingga service worker selesai diinstal
     event.waitUntil(
-        // open cache
+        // Membuka cache dengan nama 'hello-world-pwa'
         caches.open('hello-world-pwa').then(cache => {
+            // Menambahkan file-file yang akan disimpan di cache
             return cache.addAll([
-                './index.html',
-                './style.css',
-                './icons/manifest-icon-192.maskable.png',
+                './index.html', // File HTML utama aplikasi
+                './style.css', // File CSS untuk styling aplikasi
+                './icons/manifest-icon-192.maskable.png', // Ikon aplikasi
             ]);
         })
     );
 });
 
-// add event listener for fetch
+// Menambahkan event listener untuk menangani permintaan jaringan (fetch)
 self.addEventListener('fetch', event => {
-
-    // open cache
+    // Membuka cache dengan nama 'hello-world-pwa'
     caches.open('hello-world-pwa').then(cache => {
+        // Mencocokkan permintaan dengan data yang ada di cache
         return cache.match(event.request).then(response => {
+            // Jika data ditemukan di cache, gunakan data tersebut
+            // Jika tidak, lakukan fetch ke jaringan
             return response || fetch(event.request);
         });
     });
